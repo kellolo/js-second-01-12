23b66a44966f9fe490ce6f766f445bc5de309cbb
Alexander <lotos_toi@mail.ru>
2019-12-02 09:06:02 +1000

    11

--- PROJECT/Alexander Plotnikov/src/js/slider.js
+++ PROJECT/Alexander Plotnikov/src/js/slider.js
@@ -14,7 +14,7 @@ window.onload = function () {
         let iconMenu = d.querySelectorAll('.menuTop__line')
         iconMenu.forEach((el, i) => {
             iconMenu[i].classList.toggle('menuTop__line-activ')
-        }) 
+        })
         // close cart
         d.querySelector('.menuTop__CartSvg').classList.remove('menuTop__CartSvg-active')
         d.querySelector('.contCartProducts').classList.remove('contCartProducts-active')
@@ -38,7 +38,7 @@ window.onload = function () {
         let iconMenu = d.querySelectorAll('.menuTop__line')
         iconMenu.forEach((el, i) => {
             iconMenu[i].classList.remove('menuTop__line-activ')
-        }) 
+        })
 
     }
 
@@ -140,32 +140,49 @@ window.onload = function () {
     let ship = [] //ships list
     let img = [] // img list
 
-    class product {
-        constructor(title, price, brand, country, ship, img) {
+    class Product {
+        constructor(title, price, brand, country, ship, img, id) {
             this.name = title;
             this.price = price;
             this.brand = brand;
             this.country = country;
             this.ship = ship;
-            this.img = img
+            this.img = img;
+            this.id = id
         }
         render() {
             return `<div class="contItem">
                         <img src="${this.img}" width="198" height="180" alt="imgProduct" class="contItem__img">
                         <span class="contItem__name">${this.name}</span>
                         <span class="contItem__price">${this.price}</span>
-                        <button class="contItem__button">add to cart</button>
+                        <button class="contItem__button" id="${this.id}">add to cart</button>
                     </div>`
         }
     }
 
-    class contProduct {
+    class ProductForCart extends Product {
+        render() {
+            return `<div class="contCartProducts__contItem">
+                        <img src="${this.img}" width="198" height="180" alt="imgProduct"
+                            class="contCartProducts__img">
+                        <span class="contCartProducts__name">${this.name}</span>
+                        <span class="contCartProducts__price">${this.price}</span>
+                        <span class="contCartProducts__quantity">12</span>
+                        <div class="contCartProducts__buttons">
+                            <button class="contCartProducts__add">&#9650</button>
+                            <button class="contCartProducts__del">&#9660</button>
+                        </div>
+                   </div>`
+        }
+    }
+
+    class ContProduct {
         constructor(className) {
             this.className = className;
             this.arrElems = [];
         }
 
-        fetchProduct(url,callback) {
+        fetchProduct(url, callback) {
             var xhr = null
             let arr = []
             let context = this
@@ -176,13 +193,13 @@ window.onload = function () {
             }
             xhr.open('GET', url, true)
             xhr.send()
-            xhr.onreadystatechange = function () {              
+            xhr.onreadystatechange = function () {
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     let objProduct = JSON.parse(xhr.responseText)
                     objProduct.forEach(el => {
                         arr.push(el)
                     })
-                    callback.call(context,arr)
+                    callback.call(context, arr)
                 }
             }
 
@@ -190,18 +207,63 @@ window.onload = function () {
         render(mas) {
             this.arrElems = mas
             for (let i = 0; i < this.arrElems.length; i++) {
-                let Product = new product(
+                let product = new Product(
                     this.arrElems[i].name + (i + 1),
                     this.arrElems[i].price,
                     this.arrElems[i].brand,
                     this.arrElems[i].country,
                     this.arrElems[i].ship,
-                    this.arrElems[i].img)
-                d.querySelector(this.className).innerHTML += Product.render()
+                    this.arrElems[i].img,
+                    this.arrElems[i].id)
+                d.querySelector(this.className).innerHTML += product.render()
             }
         }
     }
 
-    let pageShop = new contProduct('.productsPage')
-  // pageShop.fetchProduct('http://localhost/jsonFiles/responses/catalogData.json', pageShop.render)
+    function test(arr) {
+        return arr
+    }
+
+    class Cart extends ContProduct {
+        constructor(className) {
+            super(className)
+            this.catalogData = [1, 5, 9]
+
+        }
+        render(mas) {
+            this.arrElems = mas
+            let arrProducts = []
+            for (let i = 0; i < this.arrElems.length; i++) {
+                let productt = new ProductForCart(
+                    this.arrElems[i].name + (i + 1),
+                    this.arrElems[i].price,
+                    this.arrElems[i].brand,
+                    this.arrElems[i].country,
+                    this.arrElems[i].ship,
+                    this.arrElems[i].img,
+                    this.arrElems[i].id)
+                arrProducts += productt.render()
+
+            }
+            d.querySelector(this.className).innerHTML = arrProducts
+        }
+        get() {
+            let context = this
+            this.catalogData = super.fetchProduct('./../responses/catalogData.json', test.call(context))
+            console.log(this.catalogData)
+        }
+        test(mas) {
+            return mas
+        }
+    }
+
+
+
+
+    let pageShop = new ContProduct('.productsPage')
+    pageShop.fetchProduct('./../responses/catalogData.json', pageShop.render)
+    let CartShop = new Cart('.contCartProducts__bodyCart')
+    CartShop.fetchProduct('./../responses/getBasket.json', CartShop.render)
+    CartShop.get()
+    // console.log(CartShop.catalogData)
 }

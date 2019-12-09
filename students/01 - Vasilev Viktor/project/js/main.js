6f4191532615f1857392aceb845e0dcdf39c8e72
v-v-d <v-v-d@bk.ru>
2019-12-10 02:28:15 +0300

    callbackGETRequest function making GET via callback added

--- students/01 - Vasilev Viktor/project/js/main.js
+++ students/01 - Vasilev Viktor/project/js/main.js
@@ -1,40 +1,46 @@
 //заглушки (имитация базы данных)
 const image = 'https://placehold.it/200x150';
 const cartImage = 'https://placehold.it/100x80';
-const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad'];
-const prices = [1000, 200, 20, 10, 25, 30, 18, 24];
-const ids = [1, 2, 3, 4, 5, 6, 7, 8];
+// const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad'];
+// const prices = [1000, 200, 20, 10, 25, 30, 18, 24];
+// const ids = [1, 2, 3, 4, 5, 6, 7, 8];
+
 
 class Catalog {
-    constructor (container) {
-        this.container = container;
-        this.items = [];
-        this._init ();
-    }
-    _init () {
-        this.items = fetchData ();
-        this._render ();
-    }
-    _render () {
-        let block = document.querySelector (this.container);
-        let htmlStr = '';
-        this.items.forEach (item => {
-            let prod = new CatalogItem(item);
-            htmlStr += prod.render();
-        });
-        block.innerHTML = htmlStr
-    }
+  constructor(container) {
+    this.container = container;
+    this.items = [];
+    this.fetchItems();
+  }
+
+  fetchItems() {
+    callbackGETRequest(catalogUrl, data => {
+      catalog.items = data;
+      this._render();
+    });
+  }
+
+  _render() {
+    let block = document.querySelector(this.container);
+    let htmlStr = '';
+    this.items.forEach(item => {
+      let prod = new CatalogItem(item);
+      htmlStr += prod.render();
+    });
+    block.innerHTML = htmlStr
+  }
 }
 
 class CatalogItem {
-    constructor (obj) {
-        this.product_name = obj.product_name;
-        this.price = obj.price;
-        this.id_product = obj.id_product;
-        this.img = image;
-    }
-    render () {
-        return `
+  constructor(obj) {
+    this.product_name = obj.product_name;
+    this.price = obj.price;
+    this.id_product = obj.id_product;
+    this.img = image;
+  }
+
+  render() {
+    return `
             <div class="product-item" data-id="${this.id_product}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
@@ -48,85 +54,109 @@ class CatalogItem {
                 </div>
             </div>
         `
-    }
+  }
 }
 
-let catalog = new Catalog ('.products');
+
+const URL_API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const catalogUrl = `${URL_API}/catalogData.json`;
+
+// via callback
+
+const callbackGETRequest = function (url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status !== 200) {
+        console.error('server response is not 200 OK');
+      } else {
+        const parsedData = JSON.parse(xhr.responseText);
+        callback(parsedData);
+      }
+    }
+  };
+
+  xhr.send();
+};
+
+let catalog = new Catalog('.products');
 
 //кнопка скрытия и показа корзины
 document.querySelector('.btn-cart').addEventListener('click', () => {
-    document.querySelector('.cart-block').classList.toggle('invisible');
+  document.querySelector('.cart-block').classList.toggle('invisible');
 });
 //кнопки удаления товара (добавляется один раз)
-document.querySelector('.cart-block').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('del-btn')) {
-        removeProduct (evt.target);
-    }
+document.querySelector('.cart-block').addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('del-btn')) {
+    removeProduct(evt.target);
+  }
 });
 //кнопки покупки товара (добавляется один раз)
-document.querySelector('.products').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('buy-btn')) {
-        addProduct (evt.target);
-    }
+document.querySelector('.products').addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('buy-btn')) {
+    addProduct(evt.target);
+  }
 });
 
 //создание массива объектов - имитация загрузки данных с сервера
-function fetchData () {
-    let arr = [];
-    for (let i = 0; i < items.length; i++) {
-        arr.push (createProduct (i));
-    }
-    return arr
-}
+// function fetchData() {
+//   let arr = [];
+//   for (let i = 0; i < items.length; i++) {
+//     arr.push(createProduct(i));
+//   }
+//   return arr
+// }
 
 //создание товара
-function createProduct (i) {
-    return {
-        id_product: ids[i],
-        product_name: items[i],
-        price: prices[i],
-        img: image,
-    }
-}
+// function createProduct(i) {
+//   return {
+//     id_product: ids[i],
+//     product_name: items[i],
+//     price: prices[i],
+//     img: image,
+//   }
+// }
 
 //CART
 
 // Добавление продуктов в корзину
-function addProduct (product) {
-    let productId = +product.dataset['id']; //data-id="1"
-    let find = userCart.find (element => element.id === productId); //товар или false
-    if (!find) {
-        userCart.push ({
-            name: product.dataset ['name'],
-            id: productId,
-            img: cartImage,
-            price: +product.dataset['price'],
-            quantity: 1
-        })
-    }  else {
-        find.quantity++
-    }
-    renderCart ()
+function addProduct(product) {
+  let productId = +product.dataset['id']; //data-id="1"
+  let find = userCart.find(element => element.id === productId); //товар или false
+  if (!find) {
+    userCart.push({
+      name: product.dataset ['name'],
+      id: productId,
+      img: cartImage,
+      price: +product.dataset['price'],
+      quantity: 1
+    })
+  } else {
+    find.quantity++
+  }
+  renderCart()
 }
 
 //удаление товаров
-function removeProduct (product) {
-    let productId = +product.dataset['id'];
-    let find = userCart.find (element => element.id === productId);
-    if (find.quantity > 1) {
-        find.quantity--;
-    } else {
-        userCart.splice(userCart.indexOf(find), 1);
-        document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
-    }
-    renderCart ();
+function removeProduct(product) {
+  let productId = +product.dataset['id'];
+  let find = userCart.find(element => element.id === productId);
+  if (find.quantity > 1) {
+    find.quantity--;
+  } else {
+    userCart.splice(userCart.indexOf(find), 1);
+    document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
+  }
+  renderCart();
 }
 
 //перерендер корзины
-function renderCart () {
-    let allProducts = '';
-    for (el of userCart) {
-        allProducts += `<div class="cart-item" data-id="${el.id}">
+function renderCart() {
+  let allProducts = '';
+  for (el of userCart) {
+    allProducts += `<div class="cart-item" data-id="${el.id}">
                             <div class="product-bio">
                                 <img src="${el.img}" alt="Some image">
                                 <div class="product-desc">
@@ -140,7 +170,7 @@ function renderCart () {
                                 <button class="del-btn" data-id="${el.id}">&times;</button>
                             </div>
                         </div>`
-    }
+  }
 
-    document.querySelector(`.cart-block`).innerHTML = allProducts;
+  document.querySelector(`.cart-block`).innerHTML = allProducts;
 }

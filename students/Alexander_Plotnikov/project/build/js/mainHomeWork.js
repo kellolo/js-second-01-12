e3c3d4fd0c4fe6706f58d3d54265ad05ef8b0ff8
Alexander <lotos_toi@mail.ru>
2019-12-09 23:46:59 +1000

    11

--- students/Alexander_Plotnikov/project/build/js/mainHomeWork.js
+++ students/Alexander_Plotnikov/project/build/js/mainHomeWork.js
@@ -1,10 +1,13 @@
 window.onload = function () {
 
     js1()
+
     let pageShop = new ContProduct('.productsPage')
-    // pageShop.init()
-   // pageShop.getCatalogPromis(catURL)
-      pageShop.getCatalogFetch(catURL)
+    pageShop.init()
+    let Basket = new Cart('.contCartProducts__bodyCart')
+    Basket.init()
+
+    
 }
 // ********************************* Create HTML *************************************** //
 // ********************************* Create HTML *************************************** //
@@ -15,7 +18,7 @@ let catURL = 'https://raw.githubusercontent.com/lotostoi/js-second-01-12/lesson3
 
 class Product { // класс для создания товара магазина
     constructor(obj) {
-        this.name = obj.title;
+        this.name = obj.name;
         this.price = obj.price;
         this.brand = obj.brand;
         this.country = obj.country;
@@ -28,7 +31,7 @@ class Product { // класс для создания товара магази�
         return `<div class="contItem">
                         <img src="${this.img}" width="198" height="180" alt="imgProduct" class="contItem__img">
                         <span class="contItem__name">${this.name}</span>
-                        <span class="contItem__price">$${this.price}</span>
+                        <span class="contItem__price">${this.price}</span>
                         <button class="contItem__button" data-id="${this.id}">add to cart</button>
                     </div>`
     }
@@ -42,11 +45,13 @@ class ContProduct { // класс страници магазина
     }
     init() {
         let cont = this
-        this._getCatalog(catURL, cont._render(cont, Product))
+       // this._getCatalogCallback(catURL, cont._render(cont, Product, cont.arrElems))   // получаем данные через callback
+       // this._getCatalogPromis(catURL)   // получаем данные через promis
+          this._getCatalogFetch(catURL)    // получаем данные через fetch
     }
-    _render(cont, classItem) {
+    _render(cont, classItem, Arr) {
         return function () {
-            let mas = cont.arrElems
+            let mas = Arr
             let arr = []
             for (let i = 0; i < mas.length; i++) {
                 let Prod = new classItem(mas[i])
@@ -54,9 +59,9 @@ class ContProduct { // класс страници магазина
                 arr += Prod.render()
             }
             d.querySelector(cont.className).innerHTML = arr
-         } 
+        }
     }
-    _getCatalog(url, callback) {
+    _getCatalogCallback(url, callback) {
         let cont = this
         let xhr = new XMLHttpRequest()
         xhr.open('GET', url, true)
@@ -68,7 +73,7 @@ class ContProduct { // класс страници магазина
             }
         }
     }
-    getCatalogPromis(url) {
+    _getCatalogPromis(url) {
         let cont = this
         let getData = new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest()
@@ -78,22 +83,30 @@ class ContProduct { // класс страници магазина
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     if (xhr.status === 200) {
                         resolve(JSON.parse(xhr.response))
-                    }
-                    else {
+                    } else {
                         reject('error')
                     }
                 }
             }
         })
-        getData.then(rez => { cont.arrElems = rez })
-            .finally(() => {cont._render(cont, Product)()})
+        getData.then(rez => {
+                cont.arrElems = rez
+            })
+            .finally(() => {
+                cont._render(cont, Product, cont.arrElems)()
+            })
     }
-    getCatalogFetch(url) {
+    _getCatalogFetch(url) {
         let cont = this
-        let getData = fetch(url)
-        getData.then(rez => {cont.arrElems = rez })
-            .catch(rez=> console.log(rez))
-            .finally(() => cont._render(cont, Product)())
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                cont.arrElems = data
+            }) 
+            .finally(() => {           
+                cont._render(cont, Product, cont.arrElems)()
+                console.log(cont.arrElems)
+            })
     }
 }
 
@@ -112,8 +125,18 @@ class ProductCart extends Product { // класс товара в корзине
                     </div>`
     }
 }
-
 class Cart extends ContProduct { // класс для корзины
+
+    constructor(className) {
+        super(className)
+        this.summ = []
+        this.AllQuantity = []
+    }
+
+    init(){
+        this.addToCart('.productsPage')
+    } 
+    
     addToCart(className) {
         let context = this
         //  обработчик добавления элемента в корзину
@@ -129,13 +152,14 @@ class Cart extends ContProduct { // класс для корзины
                 let elInCart = cont.CartElems.find(item => item == el)
                 if (elInCart) {
                     elInCart.quantity++
-                    cont.render(cont.CartElems, ProductCart)
+                    cont.render(cont, ProductCart, cont.CartElems)
                 } else {
                     cont.CartElems.push(el)
-                    let prodCart = new ProductCart(el.name, el.price, el.brand, el.country, el.ship, el.img, el.id)
+                    console.log(el)
+                    let prodCart = new ProductCart(el)
+                   // console.log(el)
                     d.querySelector(cont.className).innerHTML += prodCart.render()
                 }
-                // console.log(cont.CartElems)
                 cont._calcCart()
 
             }
@@ -157,7 +181,6 @@ class Cart extends ContProduct { // класс для корзины
                     }
                 })
                 cont._calcCart()
-
             }
             if (evt.target.className == 'contCartProducts__del') {
                 let parent = evt.target.parentNode
@@ -171,7 +194,7 @@ class Cart extends ContProduct { // класс для корзины
                             if (cont.CartElems.length === 0) {
                                 cont._dellCart()
                             }
-                            cont.render(cont.CartElems, ProductCart)
+                            cont.render(cont, ProductCart)
                         } else {
                             cont.CartElems[i].quantity--
                             parent.parentNode.childNodes[5].innerHTML = '$' + cont.CartElems[i].quantity * cont.CartElems[i].price
@@ -186,7 +209,6 @@ class Cart extends ContProduct { // класс для корзины
         let allSumm = 0
         let allQuantity = 0
         // расcчет суммы корзины  и общеого количества корзины
-        // console.log(this.CartElems)
         this.CartElems.forEach(e => {
             allSumm += e.price * e.quantity
             allQuantity += e.quantity
@@ -204,11 +226,6 @@ class Cart extends ContProduct { // класс для корзины
         this.arrElems.forEach(e => {
             e.quantity = 1
         })
-        // console.log(this.CartElems)
         d.querySelector(this.className).innerHTML = ''
     }
 }
-
-
-
-

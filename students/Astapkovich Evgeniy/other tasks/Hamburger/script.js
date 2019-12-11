126bea0da39e1e23737a6b803473193e1ebc4d98
Nishagunazad <ni-shagu-nazad@yandex.ru>
2019-12-11 21:51:36 +0500

    others Hamburger

---
+++ students/Astapkovich Evgeniy/other tasks/Hamburger/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const button = document.querySelector('#result-button');
+button.addEventListener('click', )
+
+class Burger {
+    constructor() {
+        this.size = size;
+        this.stuff = stuff;
+        this.toping = toping;
+    }
+
+class Parameter {
+    constructor (domEl){
+        this.name = domEl.value
+        this.price  = +domEl.dataset['price']
+        this.calories = +domEl.dataset['cals']
+    }
+}
+
+}

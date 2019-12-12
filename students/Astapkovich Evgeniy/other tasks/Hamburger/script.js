64398458ccc1441481d7fea0555db11bfa679b3e
Nishagunazad <ni-shagu-nazad@yandex.ru>
2019-12-12 15:53:48 +0500

    others Hamburger

--- students/Astapkovich Evgeniy/other tasks/Hamburger/script.js
+++ students/Astapkovich Evgeniy/other tasks/Hamburger/script.js
@@ -1,14 +1,28 @@
 'use strict';
 
-const button = document.querySelector('#result-button');
-button.addEventListener('click', )
+window.onload = () => {
+    document.querySelector('#result-button').addEventListener('click', function() {
+        let burger = new Burger('size', 'stuff', 'toping');
+        burger.showSun('#price', '#cals')
+    })
+}
 
 class Burger {
-    constructor() {
+    constructor(size, stuff, toping) {
         this.size = size;
         this.stuff = stuff;
         this.toping = toping;
     }
+
+    showSum(price, cals) {
+        document.querySelector(price).innerText = this._calc('price')
+        document.querySelector(cals).innerText = this._calc('calories')
+    }
+
+    _calc(parameterPropName) {
+
+    }
+}
 
 class Parameter {
     constructor (domEl){
@@ -16,6 +30,4 @@ class Parameter {
         this.price  = +domEl.dataset['price']
         this.calories = +domEl.dataset['cals']
     }
-}
-
 }

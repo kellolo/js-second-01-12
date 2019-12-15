d66ef5c105d3bf93bad5e5bf9c1a0c5e02209448
Nishagunazad <ni-shagu-nazad@yandex.ru>
2019-12-15 22:15:05 +0500

    закомментил заглушки

--- students/Astapkovich Evgeniy/project/js/main.js
+++ students/Astapkovich Evgeniy/project/js/main.js
@@ -4,9 +4,9 @@ const cartImage = 'https://placehold.it/100x80';
 
 // const url = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
 
-const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad', 'something'];
-const prices = [1000, 200, 20, 10, 25, 30, 18, 24, 100500];
-const ids = [1, 2, 3, 4, 5, 6, 7, 8, 000];
+// const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad', 'something'];
+// const prices = [1000, 200, 20, 10, 25, 30, 18, 24, 100500];
+// const ids = [1, 2, 3, 4, 5, 6, 7, 8, 000];
 
 class Catalog {
   constructor(container) {

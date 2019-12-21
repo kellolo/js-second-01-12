3c8ee79cbc0cb658a80806a85f571599fa7da1a5
Alexander <lotos_toi@mail.ru>
2019-12-22 09:01:45 +1000

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit a997953b55bcf05c79c944675649b214a976787e.

--- students/01 - Vasilev Viktor/project/js/main.js
+++ students/01 - Vasilev Viktor/project/js/main.js
@@ -1,201 +1,71 @@
-//заглушки (имитация базы данных)
-const image = 'https://placehold.it/200x150';
-const cartImage = 'https://placehold.it/100x80';
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+const app = new Vue({
+	el: '#app',
+	data: {
+		products: [],
+		cartItems: [],
+		isCartDisplaying: false,
+		image: 'https://placehold.it/200x150',
+		cartImage: 'https://placehold.it/100x80',
+		query: '',
+	},
+	methods: {
+		fetchProducts() {
+			return fetch(`${API}/catalogData.json`)
+				.then(response => response.json())
+				.then(products => this.products = products);
+		},
 
-class Catalog {
-  constructor(container) {
-    this.container = container;
-    this.items = [];
-    this.fetchItems();
-  }
+		fetchCart() {
+			return fetch(`${API}/getBasket.json`)
+				.then(response => response.json())
+				.then(cartItems => this.cartItems = cartItems.contents);
+		},
 
-  // // via callback
-  //
-  // callbackGETRequest(url, callback) {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.open('GET', url);
-  //
-  //   xhr.onreadystatechange = function () {
-  //     if (xhr.readyState === XMLHttpRequest.DONE) {
-  //       if (xhr.status !== 200) {
-  //         console.error('server response is not 200 OK');
-  //       } else {
-  //         const parsedData = JSON.parse(xhr.responseText);
-  //         callback(parsedData);
-  //       }
-  //     }
-  //   };
-  //
-  //   xhr.send();
-  // };
-  //
-  // fetchItems() {
-  //   const catalogUrl = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
-  //   this.callbackGETRequest(catalogUrl, data => {
-  //     catalog.items = data;
-  //     this._render();
-  //   });
-  // }
+		addProductToCart(product) {
+			fetch(`${API}/addToBasket.json`)
+				.then(response => response.json())
+				.then(data => {
+          if (data.result) {
+						console.log(`Товар ${product.product_name} добавлен в корзину`);
+					}
+				});
+		},
 
-  // // via promise
-  //
-  // promiseGETRequest(url) {
-  //   return new Promise((resolve, reject) => {
-  //     const xhr = new XMLHttpRequest();
-  //     xhr.open('GET', url);
-  //
-  //     xhr.onreadystatechange = function () {
-  //       if (xhr.readyState === XMLHttpRequest.DONE) {
-  //         if (xhr.status !== 200) {
-  //           reject('server response is not 200 OK');
-  //         } else {
-  //           resolve(xhr.responseText);
-  //         }
-  //       }
-  //     };
-  //
-  //     xhr.send();
-  //   });
-  // };
-  //
-  // fetchItems() {
-  //   const catalogUrl = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
-  //   this.promiseGETRequest(catalogUrl)
-  //     .then(data => JSON.parse(data))
-  //     .then(parsedData => {
-  //       this.items = parsedData;
-  //       this._render();
-  //     })
-  //     .catch(error => console.error(error));
-  // }
+		deleteCartItem(item) {
+			fetch(`${API}/deleteFromBasket.json`)
+				.then(response => response.json())
+        .then(data => {
+					if (data.result) {
+						console.log(`Товар ${item.product_name} удален из корзины`);
+					}
+				});
+		},
 
-  // via fetch
+		cartButtonHandler() {
+			this.isCartDisplaying = !this.isCartDisplaying;
+		},
 
-  fetchItems() {
-    const catalogUrl = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
-    fetch(catalogUrl)
-      .then(data => data.json())
-      .then(parsedData => {
-        this.items = parsedData;
-        this._render();
-      })
-      .catch(error => console.error(error));
-  }
+		buyButtonHandler(product) {
+      this.addProductToCart(product);
+		},
 
-  _render() {
-    let block = document.querySelector(this.container);
-    let htmlStr = '';
-    this.items.forEach(item => {
-      let prod = new CatalogItem(item);
-      htmlStr += prod.render();
-    });
-    block.innerHTML = htmlStr;
-  }
-}
+		deleteCartItemButtonHandler(item) {
+			this.deleteCartItem(item);
+		},
+	},
+	computed: {
+		filterProducts() {
+			return this.products.filter(product => {
+				const regexp = new RegExp(this.query, 'i');
 
-class CatalogItem {
-  constructor(obj) {
-    this.product_name = obj.product_name;
-    this.price = obj.price;
-    this.id_product = obj.id_product;
-    this.img = image;
-  }
-
-  render() {
-    return `
-            <div class="product-item" data-id="${this.id_product}">
-                <img src="${this.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${this.product_name}</h3>
-                    <p>${this.price} $</p>
-                    <button class="buy-btn" 
-                    data-id="${this.id_product}"
-                    data-name="${this.product_name}"
-                    data-image="${this.img}"
-                    data-price="${this.price}">Купить</button>
-                </div>
-            </div>
-        `
-  }
-}
-
-
-class Cart {
-  constructor(container) {
-    this.container = container;
-    this.cartItems = [];
-    this.fetchCart();
-  }
-
-  fetchCart() {
-    const cartUrl = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json';
-    fetch(cartUrl)
-      .then(data => data.json())
-      .then(parsedData => {
-        this.cartItems = parsedData.contents;
-        this._render();
-      })
-      .catch(error => console.error(error));
-  }
-
-  _render() {
-    let block = document.querySelector(this.container);
-    let htmlStr = '';
-    this.cartItems.forEach(cartItem => {
-      let itemCart = new CartItem(cartItem);
-      htmlStr += itemCart.render();
-    });
-    block.innerHTML = htmlStr;
-  }
-}
-
-
-class CartItem {
-  constructor(cartItem) {
-    this.product_name = cartItem.product_name;
-    this.price = cartItem.price;
-    this.id_product = cartItem.id_product;
-    this.quantity = cartItem.quantity;
-    this.img = cartImage;
-  }
-
-  render() {
-    return `<div class="cart-item" data-id="${this.id_product}">
-                <div class="product-bio">
-                    <img src="${this.img}" alt="Some image">
-                    <div class="product-desc">
-                        <p class="product-title">${this.product_name}</p>
-                        <p class="product-quantity">Quantity: ${this.quantity}</p>
-                        <p class="product-single-price">$${this.price} each</p>
-                    </div>
-                </div>
-                <div class="right-block">
-                    <p class="product-price">${this.quantity * this.price}</p>
-                    <button class="del-btn" data-id="${this.id_product}">&times;</button>
-                </div>
-            </div>
-        `
-  }
-}
-
-const catalog = new Catalog('.products');
-const cart = new Cart('.cart-block');
-
-
-//кнопка скрытия и показа корзины
-document.querySelector('.btn-cart').addEventListener('click', () => {
-  document.querySelector('.cart-block').classList.toggle('invisible');
+				return regexp.test(product.product_name);
+			});
+		},
+	},
+	mounted() {
+		this.fetchProducts();
+		this.fetchCart();
+	}
 });
-// //кнопки удаления товара (добавляется один раз)
-// document.querySelector('.cart-block').addEventListener('click', (evt) => {
-//   if (evt.target.classList.contains('del-btn')) {
-//     removeProduct(evt.target);
-//   }
-// });
-// //кнопки покупки товара (добавляется один раз)
-// document.querySelector('.products').addEventListener('click', (evt) => {
-//   if (evt.target.classList.contains('buy-btn')) {
-//     addProduct(evt.target);
-//   }
-// });

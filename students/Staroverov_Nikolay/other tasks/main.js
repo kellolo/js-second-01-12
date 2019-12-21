4ca6f04dd2ab8265a6f3a3180843110e9ca34444
Alexander <lotos_toi@mail.ru>
2019-12-22 09:02:26 +1000

    Revert "Revert "del all""
    This reverts commit 98d761b3db0ef76e001a7428035e93812ae3aad2.

--- students/Staroverov_Nikolay/other tasks/main.js
+++
@@ -1,5 +0,0 @@
-let textBlock = document.querySelector('p');
-let button = document.querySelector('button')
-button.addEventListener('click', () => {
-  textBlock.textContent = textBlock.textContent.replace(/'\B|\B'/g, '"');
-})

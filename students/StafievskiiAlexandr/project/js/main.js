f53e710bad0502f0bb4c958043fc94bcd96043fe
Alexander <heat@mosk.ru>
2019-12-22 00:45:10 +0300

    дз 6

--- students/StafievskiiAlexandr/project/js/main.js
+++ students/StafievskiiAlexandr/project/js/main.js
@@ -1,4 +1,4 @@
-"use strict"; 
+"use strict";
 
 let domain = "https://raw.githubusercontent.com/heatmosk/online-store-api/master";
 
@@ -10,32 +10,47 @@ let API = {
   deleteFromBasket: `${domain}/responses/deleteFromBasket.json`,
 };
 
+
 let vue = new Vue({
   el: "#app",
   data: {
-    cartVisibility: false, 
+    cartVisibility: false,
+    API: API,
     cartItems: [],
     catItems: [],
-    API: API,
   },
   methods: {
-    removeProduct(prod) {
-      prod.quantity--;
-      if (prod.quantity <= 0) {
-        this.cartItems = this.cartItems.filter(item => item.id_product !== prod.id_product)
-      } 
-      this.cartItems.splice(this.cartItems.length);
+    getJson(url) {
+      return fetch(url)
+        .then(d => d.json())
     },
-    addProduct(prod) { 
-      let p = this.cartItems.find(item => item.id_product === prod.id_product);
-      if (p) {
-        p.quantity++;
-      } else {
-        p = prod;
-        p.quantity = 1;
-        this.cartItems.splice(this.cartItems.length, 1, p); 
-      } 
-      this.cartItems.splice(this.cartItems.length);
+    removeProduct(prod) {
+      this.getJson(this.API.deleteFromBasket)
+        .then(ans => {
+          if (ans.result) {
+            let find = this.cartItems.find(item => item.id_product === prod.id_product);
+            if (find.quantity > 1) {
+              find.quantity--;
+            } else {
+              this.cartItems.splice(this.cartItems.indexOf(find), 1);
+            }
+          }
+        });
+    },
+    addProduct(prod) {
+      this.getJson(this.API.deleteFromBasket)
+        .then(ans => {
+          if (ans.result) {
+            let find = this.cartItems.find(item => item.id_product === prod.id_product);
+            if (find) {
+              find.quantity++;
+            } else { 
+              this.cartItems.push(Object.assign({}, prod, {
+                quantity: 1
+              }));
+            }
+          }
+        });
     }
   },
   computed: {
@@ -44,15 +59,17 @@ let vue = new Vue({
       for (let item of this.cartItems) {
         amount += item.price * item.quantity;
       }
-      return amount; 
+      return amount;
     },
     getCountGoods() {
       return this.cartItems.length;
     }
   },
-  beforeMounted() {
-  },
-  mounted() {
+  created() {
+    this.getJson(this.API.catalogData)
+      .then(data => {
+        this.catItems = data;
+      });
     fetch(this.API.getBasket)
       .then(resp => resp.json())
       .then(data => {
@@ -60,10 +77,57 @@ let vue = new Vue({
         this.countGoods = data.countGoods;
         this.cartItems = data.contents;
       });
-    fetch(this.API.catalogData)
-      .then(resp => resp.json())
-      .then(data => {
-        this.catItems = data;
-      });
+
+  },
+});
+
+Vue.component('catalog', { 
+  template: `
+    <div class="product-item">
+      <img :src="product.product_image_big" alt="Some img" />
+      <div class="desc">
+        <h3>{{ product.product_name }}</h3>
+        <p>{{ product.price }} $</p>
+        <button class="buy-btn" :data-id="product.id_product" :data-name="product.product_name" 
+                :data-image-big="product.product_image_big" :data-image-small="product.product_image_small"
+                :data-price="product.price" @click="catAddProduct(product)">
+          Купить
+        </button>
+      </div>
+    </div>
+  `,
+  props: ['product'],
+  methods: {
+    catAddProduct(prod) {
+      this.$root.addProduct(prod);
+    }
   }
-}); 
+});
+
+
+Vue.component('cart', {
+  template: `
+    <div class="cart-item">
+      <div class="product-bio">
+        <img :src="product.product_image_small" alt="Some image" />
+        <div class="product-desc">
+          <p class="product-title">{{ product.product_name }}</p>
+          <p class="product-quantity">Quantity: {{ product.quantity }}</p>
+          <p class="product-single-price">$ {{ product.price }} each</p>
+        </div>
+      </div>
+      <div class="right-block">
+        <p class="product-price">$ {{ product.quantity * product.price }}</p>
+        <button class="del-btn" :data-id="product.id_product" @click="cartRemoveProd(product)">
+          &times;
+        </button>
+      </div> 
+    </div> 
+  `,
+  props: ['product'],
+  methods: {
+    cartRemoveProd(prod) {
+      this.$root.removeProduct(prod);
+    }
+  }
+});

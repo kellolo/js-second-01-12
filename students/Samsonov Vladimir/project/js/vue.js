a854822ecdb93501158aabc4e6dba79dab39e6a1
Vladimir Samsonov <zerocool_90@mail.ru>
2019-12-22 03:55:43 +0300

    lesson 6

--- students/Samsonov Vladimir/project/js/vue.js
+++ students/Samsonov Vladimir/project/js/vue.js
@@ -1,15 +1,19 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// const image = 'https://placehold.it/200x150';
-// const cartImage = 'https://placehold.it/100x80';
 
-let products = new Vue ({
-    el: '.products',
+let app = new Vue ({
+    el: '#app',
 
     data: {
-        url: API_URL + '/catalogData.json',
-        img: 'https://placehold.it/200x150',
-        items: [],
+        catUrl: API_URL + '/catalogData.json',
+        cartUrl: API_URL + '/getBasket.json',
+        addUrl: API_URL + '/addToBasket.json',
+        delUrl: API_URL + '/deleteFromBasket.json',
+        catImg: 'https://placehold.it/200x150',
+        cartImg: 'https://placehold.it/100x80',
+        catItems: [],
+        cartItems: [],
+        cartShown: false
     },
 
     methods: {
@@ -17,48 +21,61 @@ let products = new Vue ({
             return fetch(url)
                 .then(d => d.json()) //возвращает объект, т.к на входе принимает JSON
         },
+
+        addItem(product) {
+            this.getItems(this.addUrl)
+                .then(ans => {
+                    if (ans.result) {
+                        let find = this.cartItems.find (item => item.id_product === product.id_product) 
+
+                        if (find) {
+                            find.quantity++
+                        } else {
+                            this.cartItems.push (Object.assign ({}, product, {quantity: 1}))
+                        }
+                    }
+                })
+        },
+
+        delItem(product) {
+            this.getItems(this.delUrl)
+                .then(ans => {
+                    if (ans.result) {
+                        let find = this.cartItems.find (item => item.id_product === product.id_product) 
+
+                        if (find.quantity > 1) {
+                            find.quantity--
+                        } else {
+                            this.cartItems.splice (this.cartItems.indexOf (find), 1)
+                        }
+                    }
+                })
+        }
     },
 
     computed: {
-
+        getSum () {
+            let sum = 0
+            let qua = 0
+            this.cartItems.forEach(el => {
+                sum += el.price * el.quantity
+                qua += el.quantity
+            })
+            return {sum, qua}
+        }
     },
 
     mounted () {
-        this.getItems(this.url)
+        this.getItems(this.catUrl)
             .then(data => {
-                this.items = data
+                this.catItems = data
             })
-            .finally(() => {console.log(this.items)})
+            .finally(() => {console.log(this.catItems)})
+
+        this.getItems(this.cartUrl)
+            .then(data => {
+                this.cartItems = data.contents
+            })
+            .finally(() => {console.log(this.cartItems)})
     }
 })
-
-let cart = new Vue ({
-    el: '.cart-block',
-
-    data: {
-        url: API_URL + '/getBasket.json',
-        img: 'https://placehold.it/200x150',
-        items: [],
-    },
-
-    methods: {
-        getItems(url) {
-            return fetch(url)
-                .then(d => d.json()) //возвращает объект, т.к на входе принимает JSON
-        },
-    },
-
-    mounted() {
-        this.getItems(this.url)
-        .then(data => {
-            this.items = data
-        })
-        .finally(() => {console.log(this.items)})
-    }
-    
-
-})
-
-document.querySelector('.btn-cart').addEventListener('click', () => {
-    document.querySelector('.cart-block').classList.toggle('invisible');
-}); 

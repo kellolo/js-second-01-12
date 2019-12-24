5d4198e26c7eaf78c8dfb2a91fa42b6419a408f3
Vladimir Samsonov <zerocool_90@mail.ru>
2019-12-25 01:24:27 +0300

    lesson 7

---
+++ students/Samsonov Vladimir/project/server/test.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer( (req, res)=> {
+    if (req.url === '/') {
+        res.write('main page')
+        res.end()
+    } else if (req.url === '/lolkek') {
+        res.write('LOL KEK')
+        res.end()
+    }
+});
+
+server.listen(3030);
+server.on('connection', ()=> {
+    console.log('connected')
+});
+console.log('listen at port 3030...')

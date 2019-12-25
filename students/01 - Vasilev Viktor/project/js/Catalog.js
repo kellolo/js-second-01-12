bd75f1e3850ecc77b0a7891eedd0e222cfc4fcee
v-v-d <v-v-d@bk.ru>
2019-12-25 15:39:03 +0300

    Catalog component updated

--- students/01 - Vasilev Viktor/project/js/Catalog.js
+++ students/01 - Vasilev Viktor/project/js/Catalog.js
@@ -1,20 +1,41 @@
 Vue.component ('catalog', {
 	template: `
 		<div class="products">
-      <CatalogItem v-if="products.length"
-							     v-for="product in products"
+      <CatalogItem v-if="filteredProducts.length"
+							     v-for="product in filteredProducts"
 							     :key="product.id_product"
 							     :product="product"
        />
-       <div v-if="!products.length">Нет данных</div>
+       <div v-if="!filteredProducts.length">Нет данных</div>
     </div>
 	`,
-	props: ['products'],
 	data () {
 		return {
+			products: [],
+			filteredProducts: [],
+			GETProductsUrl: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json',
 			POSTUrl: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/addToBasket.json',
 		};
 	},
 	methods: {
+		fetchProducts() {
+			return fetch(this.GETProductsUrl)
+				.then(response => response.json())
+				.then(products => {
+					this.products = products;
+					this.filteredProducts = products;
+				});
+		},
+
+		filterProducts(query) {
+			this.filteredProducts = this.products.filter(product => {
+				const regexp = new RegExp(query, 'i');
+
+				return regexp.test(product.product_name);
+			});
+		},
 	},
+	mounted() {
+		this.fetchProducts();
+	}
 });

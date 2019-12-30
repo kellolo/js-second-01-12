14490d25dd9955fa39cd338e1f9c6af0a499cb2e
v-v-d <v-v-d@bk.ru>
2019-12-30 03:26:50 +0300

    start new vue cli project

---
+++ students/Vasilev_Viktor/project/e-shop/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <EShop />
+  </div>
+</template>
+
+<script>
+  import EShop from './components/EShop.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      EShop,
+    },
+  }
+</script>
+
+<style lang="css">
+
+</style>
---
+++ students/Vasilev_Viktor/project/e-shop/src/components/EShop.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="e-shop">
+    <HeaderComponent ref="headercomp"/>
+
+    <CatalogComponent ref="catalog"/>
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from './HeaderComponent.vue'
+  import CatalogComponent from './CatalogComponent.vue'
+
+  export default {
+    name: 'EShop',
+    components: {
+      HeaderComponent,
+      CatalogComponent,
+    }
+  }
+</script>
+
+<style lang="css">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'SF Pro Display', sans-serif;
+  }
+
+  header {
+    display: flex;
+    background-color: #2f2a2d;
+    justify-content: space-between;
+    color: #fafafa;
+    padding: 30px 80px;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  p {
+    margin: 0 0 5px 0;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>

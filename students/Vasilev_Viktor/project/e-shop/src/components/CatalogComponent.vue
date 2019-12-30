10fa9097fc057bec7556ea0dee5ab9b33b771517
v-v-d <v-v-d@bk.ru>
2019-12-30 03:27:24 +0300

    all components added to src/components

---
+++ students/Vasilev_Viktor/project/e-shop/src/components/CatalogComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="products">
+    <div class="wrapper"
+         v-if="filteredProducts.length"
+    >
+      <CatalogItemComponent v-for="product in filteredProducts"
+                            :key="product.id_product"
+                            :product="product"
+      />
+    </div>
+    <div v-if="!filteredProducts.length">Нет данных</div>
+  </div>
+</template>
+
+<script>
+  import CatalogItemComponent from './CatalogItemComponent.vue'
+
+  export default {
+    name: "CatalogComponent",
+    data() {
+      return {
+        products: [],
+        filteredProducts: [],
+      };
+    },
+    methods: {
+      fetchProducts() {
+        return fetch('/products')
+          .then(response => response.json())
+          .then(products => {
+            this.products = products;
+            this.filteredProducts = products;
+          });
+      },
+
+      filterProducts(query) {
+        const regexp = new RegExp(query, 'i');
+        this.filteredProducts = this.products.filter(product => regexp.test(product.product_name));
+      },
+    },
+    mounted() {
+      this.fetchProducts();
+    },
+    components: {
+      CatalogItemComponent,
+    },
+  }
+</script>
+
+<style lang="css" scoped>
+  .products {
+    column-gap: 30px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 200px);
+    grid-template-rows: 1fr;
+    padding: 40px 80px;
+    justify-content: space-between;
+  }
+</style>
---
+++ students/Vasilev_Viktor/project/e-shop/src/components/CatalogItemComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="product-item">
+    <img src="../assets/product_default.png" alt="Some img">
+    <div class="desc">
+      <h3>{{ product.product_name }}</h3>
+      <p>{{ product.price }} $</p>
+      <button class="buy-btn"
+              @click="buyButtonHandler(product)"
+      >Купить
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CatalogItemComponent",
+    props: ['product'],
+    methods: {
+      buyButtonHandler(product) {
+        const cart = this.$root.$refs.headercomp.$refs.cart;
+        const currentItem = cart.getCurrentCartItem(product);
+        currentItem ? cart.updateCartItem(currentItem, currentItem.quantity + 1) : cart.addProductToCart(product);
+      },
+    },
+  }
+</script>
+
+<style lang="css" scoped>
+  .product-item {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    border-radius: 5px;
+    overflow: hidden;
+    margin: 20px 0;
+  }
+
+  .buy-btn {
+    margin-top: 5px;
+    background-color: #2f2a2d;
+    padding: 5px 15px;
+    border: 1px solid transparent;
+    color: #fafafa;
+    border-radius: 5px;
+    transition: all ease-in-out .4s;
+    cursor: pointer;
+  }
+
+  .buy-btn:hover {
+    background-color: #fafafa;
+    color: #2f2a2d;
+    border: 1px solid #2f2a2d;
+  }
+</style>

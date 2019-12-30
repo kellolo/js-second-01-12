40d7636ad1e172b2f95f55d0ce453d51e711ddc7
v-v-d <v-v-d@bk.ru>
2019-12-30 04:37:51 +0300

    server side path to static disabled (no needed when proxying)

--- students/Vasilev_Viktor/project_pre_final/server/server.js
+++ students/Vasilev_Viktor/project_pre_final/server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.json());
 
 const isProductAlreadyInCart = function (cart, productId) {

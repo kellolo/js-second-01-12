30b830d03cc7b1b43e0cbb8f68b31627cc3571f7
v-v-d <v-v-d@bk.ru>
2019-12-30 04:39:28 +0300

    App.vue updated (logic from EShop component moved)

--- students/Vasilev_Viktor/project/e-shop/src/App.vue
+++ students/Vasilev_Viktor/project/e-shop/src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-    <EShop />
+    <HeaderComponent ref="headercomp"/>
+
+    <CatalogComponent ref="catalog"/>
   </div>
 </template>
 
 <script>
-  import EShop from './components/EShop.vue'
+  import HeaderComponent from './components/HeaderComponent.vue'
+  import CatalogComponent from './components/CatalogComponent.vue'
 
   export default {
     name: 'app',
     components: {
-      EShop,
+      HeaderComponent,
+      CatalogComponent,
     },
   }
 </script>
 
 <style lang="css">
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
+  body {
+    font-family: 'SF Pro Display', sans-serif;
+  }
+
+  header {
+    display: flex;
+    background-color: #2f2a2d;
+    justify-content: space-between;
+    color: #fafafa;
+    padding: 30px 80px;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  p {
+    margin: 0 0 5px 0;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
 </style>
